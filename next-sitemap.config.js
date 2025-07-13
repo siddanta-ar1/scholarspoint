c5945a7fd0ccedba55037afe159256179f0a5571
Your Name <your_email@example.com>
2025-07-13 17:27:33 +0545

    SEO 1.

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://scholarspoint.net',
+  generateRobotsTxt: true, // (optional)
+  sitemapSize: 7000,        // optional
+}
--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -129,7 +129,7 @@ function SelectSeparator({
   return (
     <SelectPrimitive.Separator
       data-slot="select-separator"
-      className={cn("bg-border text-black pointer-events-none -mx-1 my-1 h-px", className)}
+      className={cn("bg-border  text-black pointer-events-none -mx-1 my-1 h-px", className)}
       {...props}
     />
   )
